d0872cf5778976af337c1ea93b6c9963b792aa4e
Daniel Joseph Dizon <dizonjddd18@gmail.com>
2023-10-10 13:22:33 +0800

    Added a simple factorial calculator

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-document.getElementById("title").onclick = function() {
-    alert("Hello World!");
+document.getElementById("button").onclick = function() {
+    const num1 = document.getElementById("num1").value;
+    const num2 = document.getElementById("num2").value;
+    const answer = document.getElementById("answer");
+    if(num1 && num2) {
+        answer.textContent = "Loading";
+        var formData = new FormData();
+        formData.append("num1", num1);
+        formData.append("num2", num2);
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "test.php", true);
+        xhr.onreadystatechange = function() {
+            if(xhr.readyState === 4 && xhr.status === 200) {
+                var response = xhr.responseText;
+                answer.textContent = "Answer: " + response;
+            }
+        }
+        xhr.send(formData);
+    } else {
+        alert("Please fill up both fields.");
+    }
 }
--- test.php
+++ test.php
@@ -1,5 +1,16 @@
 <?php
-  echo("This is created from github file creator");
-
-  echo("This is added from the local computer.")
+    if(isset($_POST["num1"]) && isset($_POST["num2"])) {
+        $num1 = $_POST["num1"];
+        $num2 = $_POST["num2"];
+        $factorial = $num1;
+        if($num2 == 0) {
+            $factorial = 1;
+        } else {
+            while($num2 > 1) {
+                $factorial *= $num1;
+                $num2--;
+            }
+        }
+        echo($factorial);
+    }
 ?>
